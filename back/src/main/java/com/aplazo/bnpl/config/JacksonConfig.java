61579676fbfa0e54b56c3c0958cbf6e93fa9b0e6
romelgomez <bmxandcode@gmail.com>
2025-02-23 15:34:47 -0500

    fix: disble date as timestamps

--- back/src/main/java/com/aplazo/bnpl/config/JacksonConfig.java
+++ back/src/main/java/com/aplazo/bnpl/config/JacksonConfig.java
@@ -1,23 +1,39 @@
 package com.aplazo.bnpl.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class JacksonConfig {
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addSerializer(BigDecimal.class, new MoneySerializer());
-        objectMapper.registerModule(module);
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper;
-    }
+  private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
+      .ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    SimpleModule bigDecimalModule = new SimpleModule();
+    bigDecimalModule.addSerializer(BigDecimal.class, new MoneySerializer());
+    objectMapper.registerModule(bigDecimalModule);
+
+    JavaTimeModule javaTimeModule = new JavaTimeModule();
+    javaTimeModule.addSerializer(LocalDateTime.class,
+        new LocalDateTimeSerializer(DATE_TIME_FORMATTER));
+    objectMapper.registerModule(javaTimeModule);
+
+    // Disable timestamp features
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    return objectMapper;
+  }
 }
